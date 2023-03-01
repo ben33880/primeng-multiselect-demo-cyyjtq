84a7069f2718359d55ab25dda1dd8d8ddb6383a4
ben33880 <benj.moreau33@gmail.com>
2023-03-01 20:34:34 +0100

    2 files modified, 3 added, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,65 +1,33 @@
-import { Component } from "@angular/core";
-import {
-  trigger,
-  state,
-  style,
-  transition,
-  animate
-} from "@angular/animations";
-import { SelectItem, PrimeNGConfig } from "primeng/api";
-import { CountryService } from "./countryservice";
+import { Component } from '@angular/core';
+import { SelectItem, PrimeNGConfig } from 'primeng/api';
+import { ApiService } from './api.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
-  providers: [CountryService]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  providers: [ApiService],
 })
 export class AppComponent {
-  selectedCities: string[] = [];
-
-  selectedCountries1: string[] = [];
-
-  selectedCountries2: string[] = [];
-
+  selectedUser: string[] = [];
   items: SelectItem[];
-
   item: string;
-
-  cities: any[];
-
-  countries: any[];
+  users: any[];
+  contrats: any[];
 
   constructor(
-    private countryService: CountryService,
+    private apiService: ApiService,
     private primengConfig: PrimeNGConfig
   ) {
     this.items = [];
 
-    this.countryService.getCountries().then(countries => {
-      this.items = countries;
+    this.apiService.getUsers().then((users) => {
+      this.users = users;
     });
 
-    this.countries = [
-      { name: "Australia", code: "AU" },
-      { name: "Brazil", code: "BR" },
-      { name: "China", code: "CN" },
-      { name: "Egypt", code: "EG" },
-      { name: "France", code: "FR" },
-      { name: "Germany", code: "DE" },
-      { name: "India", code: "IN" },
-      { name: "Japan", code: "JP" },
-      { name: "Spain", code: "ES" },
-      { name: "United States", code: "US" }
-    ];
-
-    this.cities = [
-      { name: "New York", code: "NY" },
-      { name: "Rome", code: "RM" },
-      { name: "London", code: "LDN" },
-      { name: "Istanbul", code: "IST" },
-      { name: "Paris", code: "PRS" }
-    ];
+    this.apiService.getContrats().then((contrats) => {
+      this.contrats = contrats;
+    });
   }
 
   ngOnInit() {
